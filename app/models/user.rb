9944780a6f12185e084463d8de8c221dbdbdbb0c
Matthew Farley <matthewthomasfarley@gmail.com>
2015-04-07 16:00:50 +0000

    Added a color scheme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,5 @@
 class User < ActiveRecord::Base
-  validate :length_of_password, on: :create
-  
   has_secure_password
   validates_presence_of :name, :email
   validates_uniqueness_of :email
-  
-  private
-  
-  def length_of_password
-    if password.length < 5
-      errors.add :password, "Password must be 5 characters or longer."
-    end
-  end  
 end
