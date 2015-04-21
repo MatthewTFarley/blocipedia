237ea0fb46ba31180714b667782fa3d1e263d8cb
Matthew Farley <matthewthomasfarley@gmail.com>
2015-04-21 12:37:40 +0000

    Adds markdown to Wikis

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -4,4 +4,22 @@ class Wiki < ActiveRecord::Base
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :user, presence: true
+  
+  def markdown_title
+    render_as_markdown title
+  end
+
+  def markdown_body
+    render_as_markdown body
+  end
+  
+  private
+  
+  def render_as_markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end
+  
 end
